56d0a8a59557de8da8cbbc53e4b4751ef2c808c5
Abirami <abibalamurugan2013@gmail.com>
2026-07-24 13:34:55 +0530

    Delete Junit directory

--- Junit/Advanced JUnit Testing/Exception Testing/ExceptionThrower.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class ExceptionThrower {
-
-    public void throwException() {
-        throw new IllegalArgumentException("Invalid input");
-    }
-}
--- Junit/Mockito/Handling Void Methods/ExternalApi.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public interface ExternalApi {
-
-    void sendData(String data);
-
-}
